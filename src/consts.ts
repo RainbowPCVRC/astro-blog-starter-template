bbe5a1d7e9e18413f3787b44e768fd77d2939fb5
RainbowVis <business.rainbowpcvrc@gmail.com>
2025-12-16 15:40:30 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Rainbow's Creations";
+export const SITE_DESCRIPTION = "Gallery • Products • Commissions • Community";
