444f8177dc925cddb8a6e87a7e88a5a6d4bc67e5
RainbowVis <business.rainbowpcvrc@gmail.com>
2025-12-16 17:52:00 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,16 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = "Rainbow's Creations";
 export const SITE_DESCRIPTION = "Gallery • Products • Commissions • Community";
+
+export const LINKS = {
+  bluesky: "https://bsky.app/profile/rainbowvis.bsky.social",
+  twitter: "https://x.com/Rainbow_Vis?s=20",
+  youtube: "https://www.youtube.com/@RainbowVis",
+  twitch: "https://www.twitch.tv/rainbow_vis",
+  gumroad: "https://rainbowscreations.gumroad.com/",
+  jinxxy: "https://jinxxy.com/rainbowvis",
+  kofi: "https://ko-fi.com/rainbow_vis",
+  discordInvite: "https://discord.com/invite/xYczE7TSMv",
+};
+
+export const TWITCH_CHANNEL = "rainbow_vis";
+export const COMMISSION_EMAIL_TO = "rainbowvis.co@gmail.com";
